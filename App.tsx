5a25640d0bdbdb310b265b6a71e1c0a3b8bbeff4
Ken <ckyamanaka@gmail.com>
2023-02-13 17:46:15 -0500

    test 5

--- App.tsx
+++ App.tsx
@@ -78,7 +78,7 @@ function App(): JSX.Element {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits. Test 12345ABC.
+            screen and then come back to see your edits. Test.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
